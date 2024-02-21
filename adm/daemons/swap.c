903a0b7353a9a0689886e46ddf429b31740d28d8
gesslar <karahd@gmail.com>
2024-02-20 19:40:41 -0500

    new swap daemon

---
+++ adm/daemons/swap.c
@@ -0,0 +1,31 @@
+// /adm/daemons/swap.c
+// This daemon exists to temporarily house data, such as when an object
+// is destructing but will be back and needs the information again.
+//
+// Created:     2024/02/20: Gesslar
+// Last Change: 2024/02/20: Gesslar
+//
+// 2024/02/20: Gesslar - Created
+
+#include <clean.h>
+
+inherit STD_DAEMON ;
+
+private nosave mapping data = ([]);
+
+void swap_in(string label, mixed value) {
+    data[label] = value ;
+}
+
+mixed swap_out(string label) {
+    mixed value = data[label] ;
+    map_delete(data, label) ;
+    return value ;
+}
+
+int clean_up(int refs) {
+    if(sizeof(data) > 0)
+        return CLEAN_LATER ;
+
+    return ::clean_up(refs) ;
+}
