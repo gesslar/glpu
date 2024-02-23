233cff790eb94cf83bc342e589423329278a4c63
gesslar <karahd@gmail.com>
2024-02-23 14:53:00 -0500

    all values must be strings via GMCP

--- std/modules/gmcp.c
+++ std/modules/gmcp.c
@@ -8,12 +8,16 @@
 
 #include <daemons.h>
 #include <classes.h>
+#include <type.h>
 
 inherit CLASS_GMCP ;
 
 // Functions from other objects
 mixed query_env(string var_name);
 int set_env(string var_name, string var_value) ;
+
+// Functions
+private mixed gmcp_stringify(mixed data) ;
 
 public int gmcp_enabled() {
     return has_gmcp(this_object()) && query_env("gmcp") == "on";
@@ -52,11 +56,51 @@ varargs void do_gmcp(string package, mixed data) {
     string message ;
 
     if(data)
-        data = json_encode(data) ;
+        data = json_encode(gmcp_stringify(data)) ;
     else
         data = "" ;
 
     message = trim(sprintf("%s %s", package, data)) ;
 
     send_gmcp(message) ;
+}
+
+mixed gmcp_stringify(mixed data) {
+    switch(typeof(data)) {
+        case T_STRING:
+            return data ;
+        case T_INT:
+            return sprintf("%d", data) ;
+        case T_ARRAY: {
+                mixed *result ;
+
+                result = map(data, (: gmcp_stringify :)) ;
+
+                return result ;
+            }
+        case T_OBJECT:
+            return "" ;
+        case T_MAPPING: {
+                mapping result = ([ ]) ;
+                mixed key, value ;
+
+                foreach(key, value in data) {
+                    result[gmcp_stringify(key)] = gmcp_stringify(value) ;
+                }
+
+                return result ;
+            }
+        case T_FUNCTION:
+            return "" ;
+        case T_FLOAT:
+            return sprintf("%f", data) ;
+        case T_BUFFER:
+            return "" ;
+        case T_CLASS:
+            return "" ;
+        default:
+            return "" ;
+    }
+
+    return "" ;
 }
