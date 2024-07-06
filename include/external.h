1111374522620693188401e74aac2fc8ef000fd8
gesslar <karahd@gmail.com>
2024-07-06 01:29:04 -0400

    adding new "environment variable" daemon

---
+++ include/external.h
@@ -0,0 +1,17 @@
+/**
+ * @file /include/external.h
+ * @description Header file for external_start()
+ *
+ * @created 2024/07/06 - Gesslar
+ * @last_modified 2024/07/06 - Gesslar
+ *
+ * @history
+ * 2024/07/06 - Gesslar - Created
+ */
+
+#ifndef __EXTERNAL_H__
+#define __EXTERNAL_H__
+
+#define EXTERNAL_ENV 1
+
+#endif // __EXTERNAL_H__
