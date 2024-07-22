a1eca8043bacd785b36639e1029139496746e913
gesslar <karahd@gmail.com>
2024-07-21 22:02:38 -0400

    updating update command

--- cmds/object/update.c
+++ cmds/object/update.c
@@ -14,82 +14,111 @@ void do_update(string file) ;
 string *collect_inherits(object obj, int depth) ;
 string *deep_collect_inherits(object obj, mapping seen, int depth) ;
 
-mixed main(object caller, string arg) {
-    object obj;
+mixed main(object tp, string arg) {
+    object obj, room;
     string error, *users;
-    string *files, file, *parts, start ;
-    int depth = 0 ;
+    string *files, file, *parts, start;
+    int depth = 0;
+    int is_room_obj;
+
+    if(!objectp(room = environment(tp)))
+        return "You are not in a valid environment.";
 
     if(arg) {
         if(arg == "-r" || arg == "-R") {
-            if(arg == "-r") depth = 1 ;
-            else if(arg == "R") depth = 2 ;
+            if(arg == "-r") depth = 1;
+            else if(arg == "-R") depth = 2;
         } else if(sizeof(parts = pcre_extract(arg, "^-([rR]) (.*)$")) == 2) {
-            if(parts[0] == "r") depth = 1 ;
-            else depth = 2 ;
+            if(parts[0] == "r") depth = 1;
+            else depth = 2;
 
-            file = parts[1] ;
+            file = parts[1];
         } else {
-            depth = 0 ;
-            file = arg ;
+            depth = 0;
+            file = arg;
         }
     }
 
     if(!file) {
-        if(!caller->query_env("cwf"))
-            return _error("You must provide an argument. Syntax: update [-rR] [<file>]") ;
-        file = caller->query_env("cwf");
+        if(!tp->query_env("cwf"))
+            return _error("You must provide an argument. Syntax: update [-rR] [<file>]");
+        file = tp->query_env("cwf");
+    } else {
+        // First, try to find an object with the given name
+        obj = present(file, tp);
+        if(!obj)
+            obj = present(file, room);
+
+        if(obj) {
+            // If we found an object, check if it's a clone
+            if(clonep(obj)) {
+                file = base_name(obj);
+            } else {
+                file = file_name(obj);
+            }
+        }
+        // If we didn't find an object, 'file' remains as the input string
     }
 
-    file = resolve_path(caller->query_env("cwd"), file);
-    file = append(file, ".c") ;
+    // At this point, 'file' should be a file name or a file path
+    file = resolve_path(tp->query_env("cwd"), file);
+
+    // Always append ".c" to the file name if it's not already there
+    file = append(file, ".c");
+
+    // Check if we're trying to update the update command itself
     if(file == append(file_name(), ".c")) {
-        return _error("You cannot update the update command. Destruct it instead.") ;
+        return _error("You cannot update the update command. Destruct it instead.");
     }
-
-    start = file ;
 
     if(file[0..<3] == VOID_ROOM) {
-        return _error("You cannot update the void object.") ;
-    }
-
-    obj = find_object(file) ;
-    if(obj) {
-        users = filter_array(all_inventory(obj), (: interactive :));
-        users->move(load_object(VOID_ROOM), 1);
-    }
-
-    if(objectp(obj)) {
-        if(virtualp(obj)) {
-            file = obj->query_virtual_master() ;
-            file = append(file, ".c") ;
-        }
-
-        obj->remove() ;
-        if(obj) destruct(obj);
+        return _error("You cannot update the void object.");
     }
 
     if(!file_exists(file))
         return _error("%s does not exist.", file);
 
-    caller->set_env("cwf", start);
+    start = file;
+
+    if(!obj) {
+        obj = find_object(file);
+    }
+
+    is_room_obj = objectp(obj) && obj->is_room();
+
+    if(objectp(obj)) {
+        if(clonep(obj)) {
+            _info(tp, "Updating master of %s", file_name(obj));
+        } else {
+            _info(tp, "Updating %s", file);
+        }
+
+        if(virtualp(obj)) {
+            file = obj->query_virtual_master();
+            file = append(file, ".c");
+        }
+
+        if(is_room_obj) {
+            users = filter_array(all_inventory(obj), (: interactive :));
+            users->move(load_object(VOID_ROOM), 1);
+            obj->remove();
+            if(obj) destruct(obj);
+        }
+        // For non-room objects, we don't destruct or move users
+    } else {
+        _info(tp, "Updating %s", file);
+    }
+
+    tp->set_env("cwf", start);
 
     obj = load_object(file);
-    files = collect_inherits(obj, depth, ([]));
-    files = map(files, (: append($1, ".c") :)) ;
-    filter(files, (: do_update :)) ;
+    files = collect_inherits(obj, depth);
+    files = map(files, (: append($1, ".c") :));
+    filter(files, (: do_update :));
 
-    obj = load_object(start) ;
-    if(pointerp(users)) users->move(obj, 1);
-    return 1 ;
-
-    error = catch(obj = load_object(file));
-
-
-    if(error)
-        return _error("Failed to load %s\n%s", file, error);
-
-    return "Successful [update]: " +  file+" was updated.\n" ;
+    obj = load_object(start);
+    if(is_room_obj && pointerp(users)) users->move(obj, 1);
+    return 1;
 }
 
 void do_update(string file, string vfile) {
@@ -159,7 +188,7 @@ string *deep_collect_inherits(object obj, mapping seen, int depth) {
     return files;
 }
 
-string help(object caller) {
+string help(object tp) {
     return
 "SYNTAX: update [-rR] [<file|here>]\n\n" +
 "If file is not specified, the current working file will be updated. If the "
