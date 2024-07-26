a4efe979c326259b66c31656328bf2f28859c54b
gesslar <karahd@gmail.com>
2024-07-26 18:08:40 -0400

    fixing pager to paginate correctly

--- std/user/pager.c
+++ std/user/pager.c
@@ -16,11 +16,11 @@
 mixed query_env(string var);
 
 // Functions
-void continue_page(string *text, mixed *cb, int no_ansi, int lines, string display, int num, int curr) ;
+void continue_page(string input, string *text, mixed *cb, int no_ansi, int lines, string display, int num, int curr) ;
 
 varargs void page(mixed text, mixed *cb, int no_ansi) {
-    int lines, sz ;
-    string display ;
+    int more_lines, sz ;
+    string page_display ;
 
     if(nullp(text))
         error("Bad argument 1 to page().") ;
@@ -34,8 +34,8 @@ varargs void page(mixed text, mixed *cb, int no_ansi) {
     if(!uniform_array(T_STRING, text))
         return 0 ;
 
-    lines = to_int(query_env("morelines")) || mud_config("MORELINES") ;
-    display = query_env("page_display") || mud_config("PAGE_DISPLAY") ;
+    more_lines = to_int(query_env("morelines")) || mud_config("MORELINES") ;
+    page_display = query_env("page_display") || mud_config("PAGE_DISPLAY") ;
 
     if(no_ansi == 1)
         no_ansi = MSG_PROMPT | NO_ANSI ;
@@ -43,39 +43,38 @@ varargs void page(mixed text, mixed *cb, int no_ansi) {
         no_ansi = MSG_PROMPT ;
 
     sz = sizeof(text) ;
-    continue_page(text, cb, no_ansi, lines, display, sz, 0) ;
+    continue_page("", text, cb, no_ansi, more_lines, page_display, sz, 0) ;
 }
 
-void continue_page(string *text, mixed *cb, int no_ansi, int lines, string display, int num, int curr) {
-    int i, j, sz ;
-    string mess ;
+void continue_page(string input, string *text, mixed *cb, int no_ansi, int more_lines, string page_display, int num, int curr) {
+    int sz, end;
+    string mess;
+    string *this_page;
 
-    if(curr >= num) {
-        if(nullp(cb)) {
-            call_back(cb) ;
-            return ;
-        }
-    }
+    // Calculate the end index for this page
+    end = min(({curr + more_lines - 1, sizeof(text) - 1}));
 
-    mess = implode(text[curr..curr + lines - 1], "\n") ;
-    text = text[curr + lines..] ;
-    curr += lines ;
+    this_page = text[curr..end];
+    mess = implode(this_page, "\n");
 
-    if(sizeof(text) > 0) {
-        switch(display) {
+    // Update curr for the next iteration
+    curr = end + 1;
+
+    if(curr < sizeof(text)) {
+        switch(page_display) {
             case "percent" :
-                mess += sprintf("[%d%% - Press <Return> to continue, q to quit]", percent(curr, num)) ;
-                break ;
+                mess += sprintf("\n[%d%% - Press <Return> to continue, q to quit]", percent(curr, num));
+                break;
             default:
-                mess += sprintf("[%d/%d - Press <Return> to continue, q to quit]", curr, num) ;
-                break ;
+                mess += sprintf("\n[%d/%d - Press <Return> to continue, q to quit]", curr, num);
+                break;
         }
 
-        tell(this_object(), mess, no_ansi) ;
-        input_to("continue_page", text, cb, no_ansi, lines, display, num, curr) ;
+        tell(this_object(), mess, no_ansi);
+        input_to("continue_page", text, cb, no_ansi, more_lines, page_display, num, curr);
     } else {
-        tell(this_object(), mess, no_ansi) ;
+        tell(this_object(), mess, no_ansi);
         if(!nullp(cb))
-            call_back(cb) ;
+            call_back(cb);
     }
 }
