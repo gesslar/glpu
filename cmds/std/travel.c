36db956abb25af3ea2438b91b8b549279e14055a
gesslar <karahd@gmail.com>
2024-09-13 19:28:21 -0400

    adding more default travel waypoints

--- cmds/std/travel.c
+++ cmds/std/travel.c
@@ -18,17 +18,17 @@ private nosave mapping destinations ;
 private nosave string *dests ;
 
 void setup() {
-    destinations = mud_config("TRAVEL_DESTINATIONS") ;
-    destinations = map(destinations, (: chop($2, ".c", -1) :)) ;
-    destinations = map(destinations, (: prepend($2, "/") :)) ;
+  destinations = mud_config("TRAVEL_DESTINATIONS") ;
+  destinations = map(destinations, (: chop($2, ".c", -1) :)) ;
+  destinations = map(destinations, (: prepend($2, "/") :)) ;
 
-    dests = keys(destinations) ;
-    dests = sort_array(dests, 1) ;
+  dests = keys(destinations) ;
+  dests = sort_array(dests, 1) ;
 
-    usage_text =
+  usage_text =
 "travel list - List all valid travel destinations.\n"
 "travel <destination> - Travel to a common destination.";
-    help_text =
+  help_text =
 "This command allows you to travel to common destinations from your current "
 "location.\n\n"
 "Valid destinations are: " + implode(dests, ", ") + ".\n\n"
@@ -39,64 +39,56 @@ void setup() {
 }
 
 mixed main(object tp, string str) {
-    string *stops ;
-    string wp_file ;
-    mixed *wps ;
-    string *wp ;
-    int num ;
-    string destination_file, destination_name ;
+  string *stops ;
+  string wp_file ;
+  mixed *wps ;
+  string *wp ;
+  int num ;
+  string destination_file, destination_name ;
 
+  if(!get_config(__RC_ENABLE_GMCP__))
+    return 0 ;
 
-    if(!get_config(__RC_ENABLE_GMCP__))
-        return 0 ;
+  if(!tp->gmcp_enabled())
+    return "You must have GMCP enabled to use this command." ;
 
-    if(!tp->gmcp_enabled())
-        return "You must have GMCP enabled to use this command." ;
+  if(!str)
+    return "Travel to where?" ;
 
-    if(!str)
-        return "Travel to where?" ;
+  if(str == "list")
+    return "Valid destinations are: " + implode(dests, ", ") + "." ;
 
-    if(str == "list")
-        return "Valid destinations are: " + implode(dests, ", ") + "." ;
-
-    if(!of(str, destinations)) {
-        printf("Not in destinations\n") ;
-        printf("Nullp: %O\n", nullp(num = to_int(str))) ;
-        if(!nullp(num = to_int(str))) {
-            printf("Is int\n") ;
-            printf("Num: %O\n", num) ;
-            wp_file = user_data_directory(query_privs(tp)) + "waypoints.txt" ;
-            if(file_exists(wp_file)) {
-                printf("File exists\n") ;
-                wps = restore_variable(read_file(wp_file)) ;
-                if(num >= 1 && num <= sizeof(wps)) {
-                    printf("Num is valid\n") ;
-                    wp = wps[num-1] ;
-                    destination_name = wp[0] ;
-                    destination_file = wp[1] ;
-                }
-            }
-        } else {
-            destination_name = str ;
-            destination_file = destinations[str] ;
-        }
+  if(!of(str, destinations)) {
+    if(!nullp(num = to_int(str))) {
+      wp_file = user_data_directory(query_privs(tp)) + "waypoints.txt" ;
+      if(file_exists(wp_file)) {
+        wps = restore_variable(read_file(wp_file)) ;
+        if(num >= 1 && num <= sizeof(wps))
+          wp = wps[num-1] ;
+        destination_name = wp[0] ;
+        destination_file = wp[1] ;
+      }
+    } else {
+      destination_name = str ;
+      destination_file = destinations[str] ;
     }
+  }
 
-    if(!destination_file)
-        return "That is not a valid destination." ;
+  if(!destination_file)
+    return "That is not a valid destination." ;
 
-    if(base_name(environment(tp)) == destination_file)
-        return "You are already at that destination." ;
+  if(base_name(environment(tp)) == destination_file)
+    return "You are already at that destination." ;
 
-    tell(tp, "Traveling to " + destination_name + "...\n") ;
+  tell(tp, "Traveling to " + destination_name + "...\n") ;
 
+  stops = ({ destination_file }) ;
+
+  if(str != "square")
+    // stops = ({ destinations["square"], destination_file }) ;
     stops = ({ destination_file }) ;
 
-    if(str != "square")
-        // stops = ({ destinations["square"], destination_file }) ;
-        stops = ({ destination_file }) ;
+  GMCP_D->send_gmcp(tp, GMCP_PKG_ROOM_TRAVEL, stops) ;
 
-    GMCP_D->send_gmcp(tp, GMCP_PKG_ROOM_TRAVEL, stops) ;
-
-    return 1 ;
+  return 1 ;
 }
